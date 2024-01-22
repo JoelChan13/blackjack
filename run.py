1e77df075c8ac9673165e26927a6ae23e38a892d
JoelChan13 <>
2024-01-22 14:22:13 +0000

    add init method and initialise variable with an empty list named self.card

--- run.py
+++ run.py
@@ -81,6 +81,19 @@ class Deck:
 
 
 class Hand:
-    def __init__(self):
+    def __init__(self, dealer=False):
         self.cards = []
-        self.cards = 0
+        self.value = 0
+        self.dealer = dealer
+
+    def add_card(self, card_list):
+        self.cards.extend(card_list)
+
+
+deck = Deck()
+deck.shuffle()
+
+hand = Hand()
+hand.add_card(deck.deal(2))
+print(hand.cards[0], hand.cards[1])
+
