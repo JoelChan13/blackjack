60ba3995c81ea737f3aa99c4c0eef729288e43a9
JoelChan13 <>
2024-01-22 10:13:58 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+####################################################################################
+
+#88          88                       88        88                       88         
+#88          88                       88        ""                       88         
+#88          88                       88                                 88         
+#88,dPPYba,  88 ,adPPYYba,  ,adPPYba, 88   ,d8  88 ,adPPYYba,  ,adPPYba, 88   ,d8   
+#88P'    "8a 88 ""     `Y8 a8"     "" 88 ,a8"   88 ""     `Y8 a8"     "" 88 ,a8"    
+#88       d8 88 ,adPPPPP88 8b         8888[     88 ,adPPPPP88 8b         8888[      
+#88b,   ,a8" 88 88,    ,88 "8a,   ,aa 88`"Yba,  88 88,    ,88 "8a,   ,aa 88`"Yba,   
+#8Y"Ybbd8"'  88 `"8bbdP"Y8  `"Ybbd8"' 88   `Y8a 88 `"8bbdP"Y8  `"Ybbd8"' 88   `Y8a  
+#                                              ,88                                  
+#                                            888P"  
+
+
+####################################################################################
+
+####################################################################################
+# Blackjack House Rules
+####################################################################################
+
+# There are no jokers present in the deck
+# The Ace card can either have a value of 11 or 1, based on the cards drawn
+# The suits Jack, Queen, and King have a value of 10
+# All cards have an equal probability of being drawn throughout the games played
+# The Computer is the dealer in this Blackjack game
+# The Dealer will keep hitting cards until the value reaches 17
+# A hand amounting to 21 is considered as Blackjack, and this is also considered a winning hand
+# If either the player or the dealer go over 21, it means they busted, and hence they lose their game
+# If no player has an amount totalling 21, the closest hand to 21 wins the game
+
+####################################################################################
+
+
