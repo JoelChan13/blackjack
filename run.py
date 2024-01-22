446d47c35f7a16d7102e8b67800a98e3aa83f112
JoelChan13 <>
2024-01-22 11:20:38 +0000

    update append command

--- run.py
+++ run.py
@@ -31,8 +31,10 @@
 import random
 
 cards = []
-suits = ['spades', 'clubs', 'hearts', 'diamonds']
-ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+suits = ["spades", "clubs", "hearts", "diamonds"]
+ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 for suit in suits:
     for rank in ranks:
-        cards.append([suits, ranks])
+        cards.append([suit, rank])
+random.shuffle(cards)
+print(cards)
