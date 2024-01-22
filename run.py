344253e7d45397d72ae56bdf0b44bb359ddd5217
JoelChan13 <>
2024-01-22 12:53:09 +0000

    create a class to group the deck

--- run.py
+++ run.py
@@ -30,51 +30,51 @@
 ###############################################################################
 import random
 
-cards = []
-suits = ["spades", "clubs", "hearts", "diamonds"]
-ranks = [
-    {"rank": "A", "value": 11},
-    {"rank": "2", "value": 2},
-    {"rank": "3", "value": 3},
-    {"rank": "4", "value": 4},
-    {"rank": "5", "value": 5},
-    {"rank": "6", "value": 6},
-    {"rank": "7", "value": 7},
-    {"rank": "8", "value": 8},
-    {"rank": "9", "value": 9},
-    {"rank": "10", "value": 10},
-    {"rank": "K", "value": 10},
-    {"rank": "Q", "value": 10},
-    {"rank": "J", "value": 10},
-]
 
-for suit in suits:
-    for rank in ranks:
-        cards.append([suit, rank])
-random.shuffle(cards)
+class Deck:
+    # a class to group the deck
+    cards = []
+    suits = ["spades", "clubs", "hearts", "diamonds"]
+    ranks = [
+        {"rank": "A", "value": 11},
+        {"rank": "2", "value": 2},
+        {"rank": "3", "value": 3},
+        {"rank": "4", "value": 4},
+        {"rank": "5", "value": 5},
+        {"rank": "6", "value": 6},
+        {"rank": "7", "value": 7},
+        {"rank": "8", "value": 8},
+        {"rank": "9", "value": 9},
+        {"rank": "10", "value": 10},
+        {"rank": "K", "value": 10},
+        {"rank": "Q", "value": 10},
+        {"rank": "J", "value": 10},
+    ]
 
-
-def shuffle():
-    """
-    a function which shuffles the cards.
-    """
+    for suit in suits:
+        for rank in ranks:
+            cards.append([suit, rank])
     random.shuffle(cards)
 
+    def shuffle():
+        """
+        a function which shuffles the cards.
+        """
+        random.shuffle(cards)
 
-def deal(number):
-    """
-    function which accepts an argument to deal more than one card and return a
-    list of cards instead of a single card, together with a for loop that will
-    add a card from the deck for each card that was dealt.
-    """
-    cards_dealt = []
-    for x in range(number):
-        card = cards.pop()
-        cards_dealt.append(card)
-    return cards_dealt
+    def deal(number):
+        """
+        function which accepts an argument to deal more than one card and return a
+        list of cards instead of a single card, together with a for loop that will
+        add a card from the deck for each card that was dealt.
+        """
+        cards_dealt = []
+        for x in range(number):
+            card = cards.pop()
+            cards_dealt.append(card)
+        return cards_dealt
 
+    shuffle()
+    card = deal(1)[0]
 
-shuffle()
-card = deal(1)[0]
-
-print(card[1]["value"])
+    print(card[1]["value"])
