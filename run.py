d0d9eaa19f46a51551b345d09a4bf6808660fc7a
JoelChan13 <>
2024-01-22 10:24:04 +0000

    create a variable for suits

--- run.py
+++ run.py
@@ -1,33 +1,34 @@
-####################################################################################
+###############################################################################
 
-#88          88                       88        88                       88         
-#88          88                       88        ""                       88         
-#88          88                       88                                 88         
-#88,dPPYba,  88 ,adPPYYba,  ,adPPYba, 88   ,d8  88 ,adPPYYba,  ,adPPYba, 88   ,d8   
-#88P'    "8a 88 ""     `Y8 a8"     "" 88 ,a8"   88 ""     `Y8 a8"     "" 88 ,a8"    
-#88       d8 88 ,adPPPPP88 8b         8888[     88 ,adPPPPP88 8b         8888[      
-#88b,   ,a8" 88 88,    ,88 "8a,   ,aa 88`"Yba,  88 88,    ,88 "8a,   ,aa 88`"Yba,   
-#8Y"Ybbd8"'  88 `"8bbdP"Y8  `"Ybbd8"' 88   `Y8a 88 `"8bbdP"Y8  `"Ybbd8"' 88   `Y8a  
-#                                              ,88                                  
-#                                            888P"  
+# 8         88                       88        88                       88         
+# 8         88                       88        ""                       88         
+# 8         88                       88                                 88         
+# 8,dPYba,  88 ,adPPYYba,  ,adPPYba, 88   ,d8  88 ,adPPYYba,  ,adPPYba, 88  ,dK  
+# 8P'   "8a 88 ""     `Y8 a8"     "" 88 ,a8"   88 ""     `Y8 a8"     "" 88 ,a8   
+# 8      d8 88 ,adPPPPP88 8b         8888[     88 ,adPPPPP88 8b         8888[      
+# 8b,  ,a8" 88 88,    ,88 "8a,   ,aa 88`"Yba,  88 88,    ,88 "8a,   ,aa 88`"YbK 
+# Y"Ybd8"'  88 `"8bbdP"Y8  `"Ybbd8"' 88   `Y8a 88 `"8bbdP"Y8  `"Ybbd8"' 88  ` K
+#                                             ,88                                  
+#                                          888P"  
 
+###############################################################################
 
-####################################################################################
-
-####################################################################################
+########################
 # Blackjack House Rules
-####################################################################################
+########################
 
 # There are no jokers present in the deck
 # The Ace card can either have a value of 11 or 1, based on the cards drawn
 # The suits Jack, Queen, and King have a value of 10
-# All cards have an equal probability of being drawn throughout the games played
+# All cards have equal probability of being drawn throughout the games played
 # The Computer is the dealer in this Blackjack game
 # The Dealer will keep hitting cards until the value reaches 17
-# A hand amounting to 21 is considered as Blackjack, and this is also considered a winning hand
-# If either the player or the dealer go over 21, it means they busted, and hence they lose their game
+# A hand amounting to 21 is considered as Blackjack,
+# If either the player or the dealer go over 21, it means they busted.
 # If no player has an amount totalling 21, the closest hand to 21 wins the game
 
-####################################################################################
+###############################################################################
 
-
+suits = [“spades”, “clubs”, “hearts”, “diamonds”]
+for suit in suits:
+    print(suit)
